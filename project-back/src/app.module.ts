af7e9ba33c89dd250be413067c11640b761490dd
Emna Cheniour <emnacheniour@insat.u-carthage.tn>
2023-05-10 00:30:45 +0100

    authentification

--- project-back/src/app.module.ts
+++ project-back/src/app.module.ts
@@ -1,11 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ArticleModule,MongooseModule.forRoot('mongodb+srv://haythemkaouech:dkRg4zopYwldpR4Y@cluster0.0se0jln.mongodb.net/test?retryWrites=true&w=majority')],
+  imports: [
+    ArticleModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+        "mongodb+srv://haythemkaouech:dkRg4zopYwldpR4Y@cluster0.0se0jln.mongodb.net/test?retryWrites=true&w=majority"
+    ),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
